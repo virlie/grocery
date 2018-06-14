cb71746f78f92ab1aa0f4e1f8b4b040f55dcdea7
Virlie Paglinawan <virliejoyp@yahoo.com>
2018-06-13 20:49:53 -0700

    put a pin

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var input = 0
+
+var input = (" "," "," ");
+var list = (" ");
+list.forEach(function(input){
+console.log(list(toUpperCase());
+});
+
+// $(document).ready(function(){
+//   $("form.list").submit(function(event){
+//     event.preventDefault();
+//   }
+// }
